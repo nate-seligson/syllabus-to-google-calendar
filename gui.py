9d57bbf55e6ea65d0530558b99dcda361ed36e18
nate-seligson <146794258+nate-seligson@users.noreply.github.com>
2024-09-18 15:57:13 -0500

    GUI in progress

---
+++ gui.py
@@ -0,0 +1,38 @@
+import dearpygui.dearpygui as dpg
+import extractor
+dpg.create_context()
+with dpg.font_registry():
+    # first argument ids the path to the .ttf or .otf file
+    default_font = dpg.add_font("Roboto-Regular.ttf", 20)
+def addTasks():
+    print("hi")
+def generate():
+    dpg.set_value(loading, "Loading...")
+    response = extractor.get_tasks(dpg.get_value(input_txt))
+    if type(response) == str:
+        dpg.set_value(loading, response)
+        return
+    tasks, parsed_tasks = response
+    dpg.set_value(loading, "")
+    for task in parsed_tasks:
+        dpg.add_text(default_value=task, bullet=True, parent = window)
+    new_button = dpg.add_button(label="Add to Google Tasks?", parent = window)
+    dpg.set_item_callback(new_button, addTasks)
+
+with dpg.window(width=500, height = 600) as window:
+    dpg.bind_font(default_font)
+    dpg.add_text(default_value = "Syllabus Task Extractor")
+    input_txt = dpg.add_input_text(
+        default_value="Copy Paste Your Syllabus Here...",
+        height=200,
+        multiline = True,
+    )
+    btn = dpg.add_button(label="Generate Tasks")
+    dpg.set_item_callback(btn, generate)
+    loading = dpg.add_text(default_value = "")
+
+dpg.create_viewport(width=800, height=600)
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
